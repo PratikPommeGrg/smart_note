de996b5774a64644565a6cf6835c52b4ddae0942
Pratik Gurung <pratikgurungsjg@gmail.com>
2024-12-27 12:31:30 +0545

    test

--- test/src/utils/split_string_test.dart
+++ test/src/utils/split_string_test.dart
@@ -6,12 +6,12 @@ void main() {
     "Split string and return list",
     () {
 //arrange
-      String str = "1,2,3,4,5,6";
+      String str = "1,2,3,4,5";
 //act
       List<String> result = SplitString.splitString(str);
 
 //assert
-      expect(result, ["1", "2", "3", "4", "5","6"]);
+      expect(result, ["1", "2", "3", "4", "5"]);
     },
   );
 }
